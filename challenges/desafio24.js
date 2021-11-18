61ccd92231c1cd0480b6406266e350f7de8114b0
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-11-18 00:14:38 -0300

    Requisitos 23, 24 e 26 concluidos

---
+++ challenges/desafio24.js
@@ -0,0 +1,5 @@
+db.voos.findOne({
+  litrosCombustivel: { $lte: 600, $exists: true },
+  "empresa.nome": { $nin: ["GOL", "AZUL"] },
+},
+  { vooId: true, "empresa.nome": true, litrosCombustivel: true, _id: false });
